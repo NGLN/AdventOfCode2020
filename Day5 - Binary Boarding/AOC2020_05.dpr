6dc9b00a6e69fe0eb8ac9359e9177d952dfc14e4
NGLN <70281601+NGLN@users.noreply.github.com>
2020-12-06 12:11:36 +0100

    Update AOC2020_05.dpr
    Simplification, because Col and Row where not needed. Still a pity Delphi has no build-in BinToInt function.
    Also, once found, stop searching Albert! ;)

--- Day5 - Binary Boarding/AOC2020_05.dpr
+++ Day5 - Binary Boarding/AOC2020_05.dpr
@@ -13,8 +13,6 @@ var
   Answer2: Integer = 0;
   I: Integer;
   J: Integer;
-  Row: Integer;
-  Col: Integer;
   Seat: Integer;
 
 begin
@@ -24,15 +22,13 @@ begin
     { Part I }
     for I := 0 to Input.Count - 1 do
     begin
-      Row := 0;
+      Seat := 0;
       for J := 0 to 6 do
         if Input[I][J + 1] = 'B' then
-          Inc(Row, 1 shl (6 - J));
-      Col := 0;
+          Inc(Seat, 1 shl (9 - J));
       for J := 0 to 2 do
         if Input[I][J + 7 + 1] = 'R' then
-          Inc(Col, 1 shl (2 - J));
-      Seat := 8 * Row + Col;
+          Inc(Seat, 1 shl (2 - J));
       Answer1 := Max(Answer1, Seat);
       Input[I] := Format('%.3d', [Seat]);
     end;
@@ -41,7 +37,10 @@ begin
     Input.Sort;
     for I := 1 to Input.Count - 1 do
       if (StrToInt(Input[I]) - StrToInt(Input[I - 1])) = 2 then
+      begin
         Answer2 := StrToInt(Input[I]) - 1;
+        Break;
+      end;
     WriteLn('Part II: ', Answer2);
   finally
     Input.Free;
