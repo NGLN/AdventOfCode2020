fe610d1ea47bfe4fc143d009eea9593d49518464
NGLN <70281601+NGLN@users.noreply.github.com>
2020-12-30 09:21:50 +0100

    Update AOC2020_19.dpr
    And now including part II!

--- Day19 - Monster Messages/AOC2020_19.dpr
+++ Day19 - Monster Messages/AOC2020_19.dpr
@@ -7,12 +7,11 @@ uses
   System.Classes,
   System.StrUtils,
   System.Generics.Collections,
-  System.Generics.Defaults,
   System.RegularExpressions;
 
 type
   TRule = class(TObject)
-    Definition: String;
+    Pattern: String;
     Index: Integer;
     Substituted: Boolean;
     function ContainsRule: Boolean; overload;
@@ -20,8 +19,7 @@ type
     procedure SubstituteRule(ARule: TRule);
   end;
 
-  TRules = class(TObjectList<TRule>)
-    function RegExPattern: String;
+  TRules = class(TObjectDictionary<Integer, TRule>)
     procedure Resolve;
     procedure SubstituteRule(ARule: TRule);
   end;
@@ -33,14 +31,14 @@ var
   C: Char;
 begin
   Result := False;
-  for C in Definition do
+  for C in Pattern do
     if CharInSet(C, ['0'..'9']) then
       Exit(True);
 end;
 
 function TRule.ContainsRule(ARule: TRule): Boolean;
 begin
-  Result := Pos(' ' + IntToStr(ARule.Index) + ' ', Definition) > 0;
+  Result := Pos(' ' + IntToStr(ARule.Index) + ' ', Pattern) > 0;
 end;
 
 procedure TRule.SubstituteRule(ARule: TRule);
@@ -50,22 +48,15 @@ var
   L: Integer;
 begin
   S := ' ' + IntToStr(ARule.Index) + ' ';
-  I := Pos(S, Definition) + 1;
+  I := Pos(S, Pattern) + 1;
   L := Length(S) - 2;
-  if Length(ARule.Definition) > 1 then
-    Definition := StuffString(Definition, I, L, '(' + ARule.Definition + ')')
+  if Length(ARule.Pattern) > 2 then
+    Pattern := StuffString(Pattern, I, L, '(' + ARule.Pattern + ')')
   else
-    Definition := StuffString(Definition, I, L, ARule.Definition);
+    Pattern := StuffString(Pattern, I, L, ARule.Pattern);
 end;
 
 { TRules }
-
-function TRules.RegExPattern: String;
-begin
-  Result := Items[0].Definition;
-  Result := StringReplace(Result, ' ', '', [rfReplaceAll]);
-  Result := '^' + Result + '$';
-end;
 
 procedure TRules.Resolve;
 var
@@ -76,9 +67,10 @@ begin
   while not Done do
   begin
     Done := True;
-    for Rule in Self do
+    for Rule in Values do
       if not Rule.Substituted and not Rule.ContainsRule then
       begin
+        Rule.Pattern := StringReplace(Rule.Pattern, ' ', '', [rfReplaceAll]);
         SubstituteRule(Rule);
         Rule.Substituted := True;
         Done := False;
@@ -92,18 +84,13 @@ var
   Rule: TRule;
 begin
   for LoopNo := 1 to 3 do // A rule appears at most 3 times in a single rule
-    for Rule in Self do
+    for Rule in Values do
       if Rule.ContainsRule(ARule) then
         Rule.SubstituteRule(ARule);
 end;
 
-function CompareRules(const Left, Right: TRule): Integer;
-begin
-  Result := Left.Index - Right.Index;
-end;
-
 var
-  Input: TStringList;
+  Messages: TStringList;
   Rules: TRules;
 
 procedure ParseInput;
@@ -111,45 +98,88 @@ var
   I: Integer;
   Rule: TRule;
 begin
-  while Input[0] <> '' do
+  while Messages[0] <> '' do
   begin
-    I := Pos(':', Input[0]);
+    I := Pos(':', Messages[0]);
     Rule := TRule.Create;
-    Rule.Index := Copy(Input[0], 1, I - 1).ToInteger;
-    Rule.Definition := ' ' + Copy(Input[0], I + 2, MaxInt) + ' ';
-    if Rule.Definition[2] = '"' then
-      Rule.Definition := Rule.Definition[3];
-    Rules.Add(Rule);
-    Input.Delete(0);
+    Rule.Index := StrToInt(Copy(Messages[0], 1, I - 1));
+    Rule.Pattern := ' ' + Copy(Messages[0], I + 2, MaxInt) + ' ';
+    if Rule.Pattern[2] = '"' then
+      Rule.Pattern := Rule.Pattern[3];
+    Rules.Add(Rule.Index, Rule);
+    Messages.Delete(0);
   end;
-  Input.Delete(0);
+  Messages.Delete(0);
+end;
+
+function ValidMessage1(const AMessage: String): Boolean;
+var
+  Pattern: String;
+  RegEx: TRegEx;
+begin
+  Pattern := '^' + Rules[0].Pattern + '$';
+  RegEx := TRegEx.Create(Pattern, [roExplicitCapture]);
+  Result := RegEx.IsMatch(AMessage);
+end;
+
+function ValidMessage2(const AMessage: String): Boolean;
+// Rule 8: 42 | 42 8
+// Rule 8: n*42              ; n>0
+// Rule 11: 42 31 | 42 11 31
+// Rule 11: m*42 m*31        ; m>0
+// Rule 0: 8 11
+// Rule 0: n*42 m*42 m*31    ; n>0, m>0
+// Rule 0: n*42 m*31         ; n>m, m>0
+var
+  Input: String;
+  Pattern: String;
+  RegEx: TRegEx;
+  M: Integer;
+begin
+  Input := AMessage;
+  M := 1;
+  repeat
+    // Pattern := '^(Pattern42){M+1,}(Pattern31){M}$'
+    Pattern := '(' + Rules[31].Pattern + '){' + IntToStr(M) + '}$';
+    RegEx := TRegEx.Create(Pattern, [roExplicitCapture]);
+    Result := RegEx.IsMatch(Input);
+    if Result then
+    begin
+      Pattern := '^(' + Rules[42].Pattern + '){' + IntToStr(M + 1) + ',}' +
+        Pattern;
+      RegEx := TRegEx.Create(Pattern, [roExplicitCapture]);
+      if RegEx.IsMatch(Input) then
+        Exit(True);
+      Inc(M);
+    end;
+  until not Result;
 end;
 
 var
-  RegEx: TRegEx;
   I: Integer;
   Answer1: Integer = 0;
+  Answer2: Integer = 0;
 
 begin
-  Input := TStringList.Create;
-  Rules := TRules.Create(TComparer<TRule>.Construct(CompareRules));
+  Messages := TStringList.Create;
+  Rules := TRules.Create;
   try
-    Input.LoadFromFile('input.txt');
+    Messages.LoadFromFile('input.txt');
     ParseInput;
-  { Part I }
-    Rules.Sort;
     Rules.Resolve;
-    RegEx := TRegEx.Create(Rules.RegExPattern, [roExplicitCapture]);
-    for I := 0 to Input.Count - 1 do
-      if RegEx.IsMatch(Input[I], 1) then
+  { Part I }
+    for I := 0 to Messages.Count - 1 do
+      if ValidMessage1(Messages[I]) then
         Inc(Answer1);
     WriteLn('Part I: ', Answer1);
   { Part II }
-
-    WriteLn('Part II: ', '');
+    for I := 0 to Messages.Count - 1 do
+      if ValidMessage2(Messages[I]) then
+        Inc(Answer2);
+    WriteLn('Part II: ', Answer2);
   finally
     Rules.Free;
-    Input.Free;
+    Messages.Free;
   end;
   ReadLn;
 end.
